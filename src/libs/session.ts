00d8f83340ef2bc79fbb3bd7cf09b5f2fd91aa0c
yamamoto-eng <78201056+yamamoto-eng@users.noreply.github.com>
2023-05-01 22:03:13 +0900

    test: session option

--- src/libs/session.ts
+++ src/libs/session.ts
@@ -2,10 +2,10 @@ import type { IronSessionOptions } from "iron-session";
 
 export const sessionOptions: IronSessionOptions = {
   password: {
-    1: process.env.COOKIE_PASSWORD_1,
-    2: process.env.COOKIE_PASSWORD_2,
+    1: "8HDplabQPHikfrG2Y4JwqoJXL7Taethf",
+    2: "8HDplabQPHikfrG2Y4JwqoJXL7Taethf",
   },
-  cookieName: process.env.COOKIE_NAME,
+  cookieName: "test-name",
   cookieOptions: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
