5aa30622a974dcbe091fb31b8e5931bf87caa356
yamamoto-eng <78201056+yamamoto-eng@users.noreply.github.com>
2023-05-02 08:22:25 +0900

    feat: Layout

--- next.config.js
+++ next.config.js
@@ -9,13 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  // env: {
-  //   CLIENT_ID: process.env.CLIENT_ID,
-  //   CLIENT_SECRET: process.env.CLIENT_SECRET,
-  //   COOKIE_PASSWORD_1: process.env.COOKIE_PASSWORD_1,
-  //   COOKIE_PASSWORD_2: process.env.COOKIE_PASSWORD_2,
-  //   COOKIE_NAME: process.env.COOKIE_NAME,
-  // },
 };
 
 module.exports = nextConfig;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,11 @@
 import { FC, PropsWithChildren } from "react";
+import { Header } from "./Header";
 
 export const Layout: FC<PropsWithChildren> = (props) => {
   const { children } = props;
   return (
     <div>
-      header
+      <Header />
       {children}
       footer
     </div>
