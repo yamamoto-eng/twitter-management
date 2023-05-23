cb78bc8598c141330a2aabf8d8c0a6491a30a35f
yamamoto-eng <78201056+yamamoto-eng@users.noreply.github.com>
2023-05-23 20:56:29 +0900

    feat: add credentials

--- src/server/db/ddbClient.ts
+++ src/server/db/ddbClient.ts
@@ -1,3 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { EnvironmentCredentials } from "aws-sdk";
 
-export const ddbClient = new DynamoDBClient({ region: "ap-northeast-1" });
+export const ddbClient = new DynamoDBClient({
+  region: "ap-northeast-1",
+  credentials: new EnvironmentCredentials("AWS"),
+});
