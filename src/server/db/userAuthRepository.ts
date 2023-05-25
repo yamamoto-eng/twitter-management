9f8ce1d239aa1696e0d559fc6dc747b44c47668a
yamamoto-eng <78201056+yamamoto-eng@users.noreply.github.com>
2023-05-25 23:38:51 +0900

    feat: create crud userauth

--- src/server/db/userAuthRepository.ts
+++ src/server/db/userAuthRepository.ts
@@ -18,7 +18,49 @@ export const userAuthRepository = () => {
     });
   };
 
+  const readUserAuth = async ({ id }: Pick<UserAuth, "id">) => {
+    return await ddbDocClient.get({
+      TableName: awsConfig.tableName,
+      Key: {
+        id,
+      },
+    });
+  };
+
+  const updateUserAuth = async ({ id, accessToken, refreshToken }: UserAuth) => {
+    const currentDate = new Date().toISOString();
+    return await ddbDocClient.update({
+      TableName: awsConfig.tableName,
+      Key: {
+        id,
+      },
+      UpdateExpression: "SET #a = :access_token, #r = :refresh_token, #u = :updated_at",
+      ExpressionAttributeNames: {
+        "#a": "access_token",
+        "#r": "refresh_token",
+        "#u": "updated_at",
+      },
+      ExpressionAttributeValues: {
+        ":access_token": accessToken,
+        ":refresh_token": refreshToken,
+        ":updated_at": currentDate,
+      },
+    });
+  };
+
+  const deleteUserAuth = async ({ id }: Pick<UserAuth, "id">) => {
+    return await ddbDocClient.delete({
+      TableName: awsConfig.tableName,
+      Key: {
+        id,
+      },
+    });
+  };
+
   return {
     createUserAuth,
+    readUserAuth,
+    updateUserAuth,
+    deleteUserAuth,
   };
 };
