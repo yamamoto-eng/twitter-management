cac36ccb51f5ab3c3470b1c36b132adc91dce712
yamamoto-eng <78201056+yamamoto-eng@users.noreply.github.com>
2023-05-31 19:52:17 +0900

    feat: add lambda permission for eventbridge

--- deployLambda.sh
+++ deployLambda.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 
+# TODO: jsファイル削除・パッケージテンプレート削除・s3削除
 esbuild ./src/lambda/*.ts --bundle --platform=node --target=es2020 --minify --outdir=./dist/lambda --format=cjs
 aws cloudformation package --s3-bucket twitter-management-lambda --template-file ./template/lambda.yml --output-template-file ./template/lambda-packaged.yml
 aws cloudformation deploy --template-file ./template/lambda-packaged.yml --stack-name twitter-management-lambda --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM
