dbb1cd3656a83b1f959103798c922327ee389912
yamamoto-eng <78201056+yamamoto-eng@users.noreply.github.com>
2023-06-09 21:18:58 +0900

    chore: hidden Duplicate atom key

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const intercept = require("intercept-stdout");
+
+function interceptStdout(text) {
+  if (text.includes("Duplicate atom key")) {
+    return "";
+  }
+  return text;
+}
+
+intercept(interceptStdout);
+
 const nextConfig = {
   reactStrictMode: false,
   images: {
