22b91568aaa10f853860f4eb61799495318fffeb
yamamoto-eng <78201056+yamamoto-eng@users.noreply.github.com>
2023-06-11 21:20:26 +0900

    feat: remove get initial props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
 import { trpc } from "@/utils/trpc";
-import { getIronSession } from "iron-session";
 import { AppType } from "next/app";
-import { NextPageContext } from "next/dist/shared/lib/utils";
-import { sessionOptions } from "@/libs";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import dayjs from "dayjs";
@@ -38,21 +35,5 @@ const App: AppType<Props> = ({ Component, pageProps }) => {
 };
 
 const AppWithTRPC = trpc.withTRPC(App);
-
-AppWithTRPC.getInitialProps = async (props): Promise<{ pageProps: Props }> => {
-  // HACK: withTRPCでラップすると内部構造が変わる
-  const ctx = (props as any).ctx as NextPageContext;
-  const { req, res } = ctx;
-
-  let pageProps = {};
-
-  if (req && res) {
-    await getIronSession(req, res, sessionOptions);
-  }
-
-  return {
-    pageProps,
-  };
-};
 
 export default AppWithTRPC;
