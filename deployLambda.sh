219a993eb8a0b18dec79bee3a045633007d19d39
yamamoto-eng <78201056+yamamoto-eng@users.noreply.github.com>
2023-06-14 17:50:43 +0900

    feat: add maximum-retry-attempts for deleteTweet lambda

--- deployLambda.sh
+++ deployLambda.sh
@@ -12,6 +12,7 @@ esbuild ./src/lambda/*.ts --bundle --platform=node --target=es2020 --minify --ou
   aws cloudformation package --s3-bucket $BUCKET_NAME --template-file ./template/lambda.yml --output-template-file $OUTPUT_TEMPLATE_YML
 aws cloudformation deploy --template-file $OUTPUT_TEMPLATE_YML --stack-name twitter-management-lambda --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM --parameter-overrides CLIENTID=$CLIENT_ID CLIENTSECRET=$CLIENT_SECRET ENCRYPTIONKEY=$ENCRYPTION_KEY APPAWSACCESSKEYID=$APP_AWS_ACCESS_KEY_ID APPAWSSECRETACCESSKEY=$APP_AWS_SECRET_ACCESS_KEY
 aws lambda update-function-event-invoke-config --function-name twitter-management-tweet --maximum-retry-attempts 0
+aws lambda update-function-event-invoke-config --function-name twitter-management-deleteTweet --maximum-retry-attempts 0
 
 aws s3 rb s3://$BUCKET_NAME --force
 rm -r ./lambda
